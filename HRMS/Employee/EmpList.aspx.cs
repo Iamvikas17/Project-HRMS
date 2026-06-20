8ece5d0fa15e82cf02c2b1a31a9c74c88cf38c7b
Vikas Yadav <vikas.vikasyadav2005@gmail.com>
2026-06-20 23:07:19 +0530

    Added Admin master page changes and Employee module

---
+++ HRMS/Employee/EmpList.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HRMS.Employee
+{
+    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+    public partial class EmpList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
+    }
+}
